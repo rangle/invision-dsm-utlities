5fb6ec574c8e3d5f5a8950d16b03c1474a50e2b8
chrisdepaul <chris.depaul@rangle.io>
2020-01-26 20:56:01 +0100

    refactor download-design-tokens

--- src/download-design-tokens.ts
+++ src/download-design-tokens.ts
@@ -1,42 +1,44 @@
 #!/usr/bin/env node
 
-/**
- *
- * Use this script in your package.json to download the latest design tokens from InVision DSM
- *
- * Script Parameters:
- * url=<InVision Design Tokens URL>
- * writePath=<Relative file path to write design tokens>
- *
- * Example:
- * "scripts": {
- *     "fetch-design-tokens": "node ./fetch-design-tokens url=https://rangle.invisionapp.com/dsm-export/rangle-io/where-van-gogh/_style-params.css?key=Hk4MnZ1bU writePath=/src/_style-params.css"
- * }
- */
-
 import fs from 'fs';
 import https from 'https';
 
 import minimist from 'minimist';
-const { s: source, d: destination } = minimist(process.argv.slice(2));
+
+// Event Handlers
+const errorHandler = (e: any) => {
+    console.error(e);
+    process.exit(0);
+};
+
+const finishHandler = () => {
+    console.log(`Design Tokens successfully downloaded.`)
+};
+
+// Handle CLI input
+const {s: source, d: destination} = minimist(process.argv.slice(2));
 
 if (!source) {
-    console.error('Missing InVision URL (e.g. -s https://...)');
-    process.exit(0);
+    errorHandler('Missing InVision URL (e.g. -s https://...)');
 }
 
 if (!destination) {
-    console.error('Missing write path (e.g. -d /my.css)');
-    process.exit(0);
+    errorHandler('Missing write path (e.g. -d /my.css)');
 }
 
+// Write response to file
+const writeResponseToFile = (filePath: string) => (res: any) => {
+    if (res.statusCode !== 200) {
+        return errorHandler('Network request failed.');
+    }
+
+    res
+        .setEncoding('utf8')
+        .pipe(fs.createWriteStream(filePath))
+        .on('error', errorHandler)
+        .on('finish', finishHandler)
+};
+
 https
-    .get(source, (res: any) => {
-        if(res.statusCode !== 200) {
-            return;
-        }
-        res.setEncoding('utf8');
-        res
-            .pipe(fs.createWriteStream(destination))
-            .on('finish', () => console.log(`Design Tokens downloaded successfully and written to ${destination}.`))
-    });
+    .get(source, writeResponseToFile(destination))
+    .on('error', errorHandler);
