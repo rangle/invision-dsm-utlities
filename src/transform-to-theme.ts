8b22f1c36b521d407c8a3e5bfa135b9409fa8f74
chrisdepaul <chris.depaul@rangle.io>
2020-01-26 21:00:30 +0100

    clean up transform-to-theme

--- src/transform-to-theme.ts
+++ src/transform-to-theme.ts
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-/**
- *
- * Next Steps:
- * 4. Create a couple theme based transformations and output file
- */
-
 import fs from 'fs';
 import minimist from 'minimist';
 import * as babel from '@babel/core';
 
-const parsedArgs = minimist(process.argv.slice(2));
 const callingDir = process.cwd();
+
+// Event Handlers
+const errorHandler = (e: any) => {
+    console.error(e);
+    process.exit(0);
+};
 
 // Theme Transformations
 const { colors } = require('./transforms/colors');
 const { fontSizes } = require('./transforms/fontSizes');
 
 // Handle CLI input
+const parsedArgs = minimist(process.argv.slice(2));
 const { s: source, d: destination } = parsedArgs;
 let sourceTrimmed = source[0] === '.' ? source.slice(1) : source;
 
@@ -41,16 +41,14 @@ babel.transform(code, {
     plugins: ['codegen'],
 }, (err: any, result: any) => {
     if (err) {
-        console.error(err);
-        process.exit(0);
+        errorHandler(err);
     }
 
     const codeString = result.code.replace('module.exports = ', 'export default ');
 
     fs.writeFile(destination, codeString, (err: any) => {
         if (err) {
-            console.error(`Failed to create ${destination}.`);
-            process.exit(0);
+            errorHandler(`Failed to create ${destination}.`)
         }
 
         console.log(`Successfully created ${destination}.`);
