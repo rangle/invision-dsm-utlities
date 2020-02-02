a363b361562c5c15b0073449d17e878cc3fb22f5
chrisdepaul <chris.depaul@rangle.io>
2020-02-02 09:46:59 +0100

    add a react example

---
+++ examples/react-exampe.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-system';
+
+import themeDSM from './output/theme.dsm';
+
+// You could add additional values to themeDSM by
+// extending the object
+
+const theme = {
+  ...themeDSM,
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+};
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <div>Example</div>
+      </ThemeProvider>
+  );
+  }
+}
+
+export default App;
